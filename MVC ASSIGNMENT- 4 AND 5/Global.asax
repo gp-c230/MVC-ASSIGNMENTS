f87748bd0fd3b94a64c7c98e4a53ab7c6e67cc30
gp-c230 <102417125+gp-c230@users.noreply.github.com>
2022-05-04 06:21:55 +0000

    Add files via upload

---
+++ MVC ASSIGNMENT- 4 AND 5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Assignment_4_New.MvcApplication" Language="C#" %>
---
+++ MVC ASSIGNMENT-6/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Day6MvcApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day6MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<User> users = new List<User>();
+        public ActionResult GetServerTime()
+        {
+            System.Threading.Thread.Sleep(5000);
+            return PartialView();
+        }
+        public ActionResult GetAllUsers()
+        {
+            return PartialView(users);
+        }
+
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(User u)
+        {
+            users.Add(u);
+            return RedirectToAction("GetAllUsers");
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
